d6c625cee700b41e420168966b0406a28208b4a5
TanQiHan <tanqihanhan@gmail.com>
2018-01-07 01:52:23 +0800

    register store into database

--- app/src/main/java/my/edu/tarc/assignment/PINSetupActivity.java
+++ app/src/main/java/my/edu/tarc/assignment/PINSetupActivity.java
@@ -1,25 +1,71 @@
 package my.edu.tarc.assignment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.goodiebag.pinview.Pinview;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import my.edu.tarc.assignment.Model.User;
+
+import static my.edu.tarc.assignment.R.string.btnconfirm;
+
 public class PINSetupActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pinsetup);
-        Button btnconfirm=(Button)findViewById(R.id.buttonConfirm);
-        final Pinview pinview=(Pinview)findViewById(R.id.pinviewTransactionPIN);
-        btnconfirm.setOnClickListener(new View.OnClickListener() {
+
+        Button buttonnconfirm=(Button)findViewById(R.id.buttonConfirm);
+
+        buttonnconfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Pinview PIN=(Pinview)findViewById(R.id.pinviewTransactionPIN);
+            if(PIN.getValue().length()<6){
+                Toast.makeText(getApplicationContext(),"Error:"+"Please fill up 6-Digit PIN.",Toast.LENGTH_SHORT).show();
+            }else{
+                String username,password,phoneNo,email,name,transactionPIN;
+                Intent intent=getIntent();
+                username=intent.getStringExtra(RegisterActivity.USER_USERNAME);
+                password=intent.getStringExtra(RegisterActivity.USER_PASSWORD);
+                email=intent.getStringExtra(RegisterActivity.USER_EMAIL);
+                phoneNo=intent.getStringExtra(RegisterActivity.USER_PHONENO);
+                name=intent.getStringExtra(RegisterActivity.USER_NAME);
+                transactionPIN=PIN.getValue().toString();
+                User user=new User(username,password,name,phoneNo,email,Integer.parseInt(transactionPIN),0.0);
+
+                try {
+                    InsertUser(PINSetupActivity.this, getString(R.string.insert_user_url), user);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
 
 
+
+            }
 
 
 
@@ -28,4 +74,68 @@ public class PINSetupActivity extends AppCompatActivity {
 
 
     }
+    public void InsertUser(Context context,String url,final User user){
+        //mPostCommentResponse.requestStarted();
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        //Send data
+        try {
+            StringRequest postRequest = new StringRequest(
+                    Request.Method.POST,
+                    url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            JSONObject jsonObject;
+                            try {
+                                jsonObject = new JSONObject(response);
+                                int success = jsonObject.getInt("success");
+                                String message = jsonObject.getString("message");
+                                if (success==0) {
+                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                                }else{
+                                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                                    finish();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), "Error. " + error.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("username",user.getUsername());
+                    params.put("password",user.getPassword());
+                    params.put("name", user.getName());
+                    params.put("phoneNo", user.getPhoneNo());
+                    params.put("email", user.getEmail());
+                    params.put("pin",String.valueOf(user.getPin()));
+                    params.put("balance",String.valueOf(user.getBalance()));
+
+                    return params;
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+                    return params;
+                }
+            };
+            queue.add(postRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
--- app/src/main/java/my/edu/tarc/assignment/RegisterActivity.java
+++ app/src/main/java/my/edu/tarc/assignment/RegisterActivity.java
@@ -1,41 +1,205 @@
 package my.edu.tarc.assignment;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
+import my.edu.tarc.assignment.Model.User;
+
+import static my.edu.tarc.assignment.R.id.editTextUsername;
 
 public class RegisterActivity extends AppCompatActivity {
-    EditText editTextUsername,editTextPassword,editTextConfirmPassword,editTextFullName,editTextPhoneNumber,editTextEmailAddress;
+    public static final String TAG = "my.edu.tarc.assignment";
+    private List<User> userList;
+    private ProgressDialog pDialog;
+    private RequestQueue queue;
+    public final static String USER_USERNAME="username";
+    public final static String USER_PASSWORD="password";
+    public final static String USER_NAME="name";
+    public final static String USER_PHONENO="phoneNo";
+    public final static String USER_EMAIL="email";
+
+    EditText editTextUsername2,editTextPassword,editTextConfirmPassword,editTextFullName,editTextPhoneNumber,editTextEmailAddress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        pDialog = new ProgressDialog(RegisterActivity.this);
+        userList = new ArrayList<>();
 
-
-        editTextUsername=(EditText)findViewById(R.id.editTextUsername);
-        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        editTextUsername2=(EditText)findViewById(R.id.editTextUsername2);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword2);
         editTextConfirmPassword=(EditText)findViewById(R.id.editTextConfirmPassword);
         editTextFullName=(EditText)findViewById(R.id.editTextFullName);
         editTextPhoneNumber=(EditText)findViewById(R.id.editTextPhoneNumber);
         editTextEmailAddress=(EditText)findViewById(R.id.editTextEmailAddress);
-        editTextUsername.requestFocus();
+        editTextUsername2.requestFocus();
         Button nextButton=(Button)findViewById(R.id.buttonNext);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                if(!isEmptyField()){
+                    getUser(getApplicationContext(), getString(R.string.get_user_url));
+                    if(!CheckDuplicateUsername()){
+                        if(CheckPassword()){
+                            Intent intent=new Intent(RegisterActivity.this,PINSetupActivity.class);
+                            String username=editTextUsername2.getText().toString();
+                            String password=editTextPassword.getText().toString();
+                            String phoneNo=editTextPhoneNumber.getText().toString();
+                            String email=editTextEmailAddress.getText().toString();
+                            String name=editTextFullName.getText().toString();
+                            intent.putExtra(USER_USERNAME,username);
+                            intent.putExtra(USER_PASSWORD,password);
+                            intent.putExtra(USER_NAME,name);
+                            intent.putExtra(USER_EMAIL,email);
+                            intent.putExtra(USER_PHONENO,phoneNo);
+                            startActivity(intent);
+
+                        }
+                    }
+                }
 
 
 
-
-
-
-                Intent intent=new Intent(RegisterActivity.this,PINSetupActivity.class);
-                startActivity(intent);
             }
         });
     }
+    public boolean isEmptyField(){
+        boolean isEmpty=false;
+        String username = editTextUsername2.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String confirmpassword = editTextConfirmPassword.getText().toString();
+        String email = editTextEmailAddress.getText().toString();
+        String name = editTextFullName.getText().toString();
+        String phoneNo = editTextPhoneNumber.getText().toString();
+        if(username.isEmpty()){
+            isEmpty=true;
+            Toast.makeText(getApplicationContext(),"Error:"+"Username cannot be empty.",Toast.LENGTH_SHORT).show();
+
+        } else if (password.isEmpty()) {
+            isEmpty=true;
+            Toast.makeText(getApplicationContext(),"Error:"+"Password cannot be empty.",Toast.LENGTH_SHORT).show();
+
+        } else if (confirmpassword.isEmpty()) {
+            isEmpty=true;
+            Toast.makeText(getApplicationContext(),"Error:"+"Confirm Password cannot be empty.",Toast.LENGTH_SHORT).show();
+
+        } else if (name.isEmpty()) {
+                isEmpty = true;
+                Toast.makeText(getApplicationContext(), "Error:" + "Name cannot be empty.", Toast.LENGTH_SHORT).show();
+
+            } else if (phoneNo.isEmpty()) {
+                isEmpty = true;
+                Toast.makeText(getApplicationContext(), "Error:" + "Phone Number cannot be empty.", Toast.LENGTH_SHORT).show();
+
+            } else if (email.isEmpty()) {
+                isEmpty = true;
+                Toast.makeText(getApplicationContext(), "Error:" + "Email cannot be empty.", Toast.LENGTH_SHORT).show();
+
+
+        }
+
+        return isEmpty;
+    }
+
+    public void getUser(Context context,String url){
+        queue = Volley.newRequestQueue(context);
+
+        if (!pDialog.isShowing())
+            pDialog.setMessage("processing...");
+        pDialog.show();
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(
+                url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            userList.clear();
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject userResponse = (JSONObject) response.get(i);
+                                String username = userResponse.getString("username");
+                                String password = userResponse.getString("password");
+                                String name = userResponse.getString("name");
+                                String phoneNo = userResponse.getString("phoneNo");
+                                String email = userResponse.getString("email");
+                                int pin = userResponse.getInt("pin");
+                                double balance = userResponse.getDouble("balance");
+                                User user = new User(username,password,name,phoneNo,email,pin, balance);
+                                userList.add(user);
+                            }
+                            CheckDuplicateUsername();
+                            if (pDialog.isShowing())
+                                pDialog.dismiss();
+                        } catch (Exception e) {
+                            Toast.makeText(getApplicationContext(), "Error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Toast.makeText(getApplicationContext(), "Error" + volleyError.getMessage(), Toast.LENGTH_LONG).show();
+                        if (pDialog.isShowing())
+                            pDialog.dismiss();
+                    }
+                });
+
+        // Set the tag on the request.
+        jsonObjectRequest.setTag(TAG);
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
+    }
+    public boolean CheckDuplicateUsername(){
+        String username = editTextUsername2.getText().toString();
+    boolean isDuplicate=false;
+            for (int i = 0; i < userList.size(); i++) {
+                if (username.equalsIgnoreCase(userList.get(i).getUsername())) {
+
+                    Toast.makeText(getApplicationContext(), "Error:" + "This username already exist.", Toast.LENGTH_SHORT).show();
+                    isDuplicate=true;
+
+                }
+            }
+
+        return isDuplicate;
+    }
+    public boolean CheckPassword(){
+        String password = editTextPassword.getText().toString();
+        String confirmpassword = editTextConfirmPassword.getText().toString();
+        boolean isValid = false;
+
+
+            if(password.equals(confirmpassword)){
+                isValid=true;
+            }else{
+                Toast.makeText(getApplicationContext(),"Error:"+"ConfirmPassword must same with Password.",Toast.LENGTH_SHORT).show();
+            }
+
+
+
+    return isValid;
+    }
+
+
+
 }
