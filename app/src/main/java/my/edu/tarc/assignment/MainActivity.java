6e87f201440b58dd31e6e36a52504ed8e2758fb1
TanQiHan <tanqihanhan@gmail.com>
2018-01-08 19:09:17 +0800

    QRcode scanner half done (proceed payment fail)

--- app/src/main/java/my/edu/tarc/assignment/MainActivity.java
+++ app/src/main/java/my/edu/tarc/assignment/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -117,6 +118,15 @@ public class MainActivity extends AppCompatActivity
         imageButtonPlaystation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+            }
+        });
+        Button buttonScan = (Button)findViewById(R.id.buttonScan);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,QRCodeScannerActivity.class);
+                startActivity(intent);
 
             }
         });
